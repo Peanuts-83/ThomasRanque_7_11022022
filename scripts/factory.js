3ebf0b74f39d84769a30328b2a52770c9c888854
Peanuts-83 <tranque@free.fr>
2022-02-20 06:52:42 +0100

    header UI ok

--- scripts/factory.js
+++ scripts/factory.js
@@ -1,88 +1,91 @@
 // TAGS Factory
 class Tag {
-  static #idNum = 0;
-  constructor(type, txt) {
-    this.type = type;
-    this.txt = txt;
-    this.color = this.defColor();
-    this.idNum = Tag.#idNum++;
-  }
-  defColor() {
-    switch (this.type) {
-      case 'ingredients':
-        return 'primary';
-      case 'appareils':
-        return 'success';
-      case 'ustensiles':
-        return 'danger';
-      default:
-        console.log('ERROR with TAG type constructor!');
+    static #idNum = 0;
+    constructor(type, txt) {
+        this.type = type;
+        this.txt = txt;
+        this.color = this.defColor();
+        this.idNum = Tag.#idNum++;
     }
-  }
-  make() {
-    const tag = document.createElement('button');
-    tag.classList = `btn rounded d-inline text-white bg-${this.color}`;
-    tag.id = `id#${this.idNum}`;
-    tag.innerHTML = `${this.txt} <span><i class="far fa-times-circle ms-2"></i></span>`;
-    return tag;
-  }
+    defColor() {
+        switch (this.type) {
+            case 'ingredients':
+                return 'primary';
+            case 'appareils':
+                return 'success';
+            case 'ustensiles':
+                return 'danger';
+            default:
+                console.log('ERROR with TAG type constructor! Define new search type or check type...');
+        }
+    }
+    make() {
+        const tag = document.createElement('button');
+        tag.classList = `btn w-auto rounded d-flex align-items-center text-white bg-${this.color}`;
+        tag.id = `id#${this.idNum}`;
+        tag.innerHTML = `${this.txt} <span><i class="far fa-times-circle ms-2"></i></span>`;
+        tag.addEventListener('click', closeTag);
+        console.log(tag)
+        return tag;
+    }
 }
 
 
 //BTN SUB SEARCH Factory
 class BtnSubSearch {
-  constructor(type) {
-    this.type = type;
-    this.color = this.defColor();
-  }
-  defColor() {
-    switch (this.type) {
-      case 'ingredients':
-        return 'primary';
-      case 'appareils':
-        return 'success';
-      case 'ustensiles':
-        return 'danger';
-      default:
-        console.log('ERROR with BTN type constructor!');
+    constructor(type) {
+        this.type = type;
+        this.color = this.defColor();
     }
-  }
-  makeInactive() {
-    const container = document.createElement('div');
-    const btn1 = document.createElement('button');
-    const btn2 = document.createElement('button');
-    container.classList = `col-1 input-group ${this.type} inactive`;
-    btn1.classList = `form-control form-control-lg py-4 rounded-left border-0 text-white bg-${this.color}`;
-    btn1.innerText = this.type[0].toUpperCase() + this.type.substr(1);
-    btn2.classList = `input-group-text px-3 rounded-right border-0 text-white bg-${this.color}`;
-    btn2.innerHTML = '<i class="fas fa-chevron-down"></i>';
-    container.appendChild(btn1);
-    container.appendChild(btn2);
-    return container;
-  }
-  makeActive() {
-    const container = document.createElement('div');
-    const divQ = document.createElement('div');
-    const input = document.createElement('input');
-    const btn = document.createElement('button');
-    const divR = document.createElement('div');
-    container.className = `row rounded bg-danger ${this.type} active`;
-    divQ.classList = `col input-group`;
-    input.classList = `form-control form-control-lg py-4 rounded-left border-0 text-white bg-${this.color}`;
-    input.type = 'text';
-    input.name = this.type;
-    console.log(this.type)
-    input.placeholder = `Rechercher un ${this.type.substr(0, this.type.length-2)}`;
-    btn.classList = `input-group-text px-3 rounded-right border-0 text-white bg-${this.color}`;
-    btn.innerHTML = '<i class="fas fa-chevron-up"></i>';
-    divQ.appendChild(input);
-    divQ.appendChild(btn);
-    divR.classList = `row rounded-bottom border-0 text-white bg-${this.color} result`;
-    container.appendChild(divQ);
-    container.appendChild(divR);
-    return container;
-  }
-  make() {
-    return this.makeInactive();
-  }
+    defColor() {
+        switch (this.type) {
+            case 'ingredients':
+                return 'primary';
+            case 'appareils':
+                return 'success';
+            case 'ustensiles':
+                return 'danger';
+            default:
+                console.log('ERROR with BTN type constructor! Define new search type or check type...');
+        }
+    }
+    makeInactive() {
+        const container = document.createElement('div');
+        const btn1 = document.createElement('button');
+        const btn2 = document.createElement('button');
+        container.classList = `col-1 px-0 input-group ${this.type}`;
+        container.id = 'inactive';
+        btn1.classList = `form-control form-control-lg py-4 rounded-left border-0 text-white bg-${this.color}`;
+        btn1.innerText = this.type[0].toUpperCase() + this.type.substr(1);
+        btn2.classList = `input-group-text px-3 rounded-right border-0 text-white bg-${this.color}`;
+        btn2.innerHTML = '<i class="fas fa-chevron-down"></i>';
+        container.appendChild(btn1);
+        container.appendChild(btn2);
+        return container;
+    }
+    makeActive() {
+        const container = document.createElement('div');
+        const divQ = document.createElement('div');
+        const input = document.createElement('input');
+        const btn = document.createElement('button');
+        const divR = document.createElement('div');
+        container.className = `row rounded bg-${this.color} ${this.type}`;
+        container.id = 'active';
+        divQ.classList = `col input-group`;
+        input.classList = `form-control form-control-lg py-4 rounded-left border-0 text-white bg-${this.color}`;
+        input.type = 'text';
+        input.name = this.type;
+        input.placeholder = `Rechercher un ${this.type.substr(0, this.type.length - 1)}`;
+        btn.classList = `input-group-text px-3 rounded-right border-0 text-white bg-${this.color}`;
+        btn.innerHTML = '<i class="fas fa-chevron-up"></i>';
+        divQ.appendChild(input);
+        divQ.appendChild(btn);
+        divR.classList = `row rounded-bottom border-0 text-white bg-${this.color} result`;
+        container.appendChild(divQ);
+        container.appendChild(divR);
+        return container;
+    }
+    make() {
+        return [this.makeInactive(), this.makeActive()];
+    }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,62 @@
-// DOM ELEMENTS
-const search = document.querySelector('.mainSearch form');
-const submit = search.querySelector('button');
-const tags = document.querySelector('.tags');
+// DOM ELEMENT REQUIRED FOR INIT
 const subSearch = document.querySelector('.subSearch');
-const ingredients = document.querySelectorAll('.ingredients');
-const appareils = document.querySelectorAll('.appareils');
-const ustensiles = document.querySelectorAll('.ustensiles');
-// TODO: ADD RESPONSE CARDS
+const tags = document.querySelector('.tags');
+
 
 // INIT BUILD HEADER
 function init() {
-  const subSearchValues = ['ingredients', 'appareils', 'ustensiles'];
-  for (let type of subSearchValues) {
-    const subBtn = new BtnSubSearch(type);
-    console.log(subBtn)
-    // subBtn.forEach(btn => subSearch.append(btn));
-    subSearch.appendChild(subBtn.make())
-  }
+    const subSearchValues = ['ingredients', 'appareils', 'ustensiles'];
+    for (let type of subSearchValues) {
+        const subBtn = new BtnSubSearch(type);
+        subBtn.make().forEach(btn => subSearch.append(btn));
+    }
+    // FAKE TAG
+    const tag = new Tag('appareils', 'four').make();
+    tags.appendChild(tag);
+    const tag2 = new Tag('ingredients', 'olive de marengo').make();
+    tags.appendChild(tag2);
 }
 
 init();
+
+
+// DOM ELEMENTS initiated after DOM BUILD
+const search = document.querySelector('.mainSearch form');
+const submit = search.querySelector('button');
+const ingredients = subSearch.querySelectorAll('.ingredients');
+const appareils = subSearch.querySelectorAll('.appareils');
+const ustensiles = subSearch.querySelectorAll('.ustensiles');
+const active = subSearch.querySelectorAll('#active');
+const inactive = subSearch.querySelectorAll('#inactive');
+// TODO: ADD RESPONSE CARDS
+
+
+//////////
+// TAGS //
+function closeTag(e) {
+    console.log(e);
+
+}
+
+
+///////////////
+// BTNS SWAP //
+inactive.forEach(btn => btn.addEventListener('click', swapSubSearch));
+active.forEach(btn => btn.querySelector('button').addEventListener('click', swapSubSearch));
+
+function swapSubSearch(e) {
+    // ALL BTNS INACTIVE
+    [...active].forEach(elt => elt.style.display = 'none');
+    [...inactive].forEach(elt => elt.style.display = 'flex');
+    // ONE BTN ACTIVE
+    const [container] = [...active].filter(elt => elt.contains(e.target)).length > 0
+        ? [...active].filter(elt => elt.contains(e.target))
+        : [...inactive].filter(elt => elt.contains(e.target));
+    console.log(container)
+    const isActive = container.id;
+    const type = container.classList[container.classList.length - 1];
+    const [siblingContainer] = [...subSearch.querySelectorAll(`.${type}`)].filter(elt => elt.id != isActive);
+
+    container.style.display = 'flex' ? 'none' : 'flex';
+    siblingContainer.style.display = container.style.display == 'flex' ? 'none' : 'flex';
+}
